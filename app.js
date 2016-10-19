2877e7dd37591cbacaa9c0f70e5db35311341573
Doug Riblet <smashzen85@gmail.com>
2016-10-19 12:48:00 -0500

    basic express server setup

--- app.js
+++ app.js
@@ -4,3 +4,21 @@ var moment = require("moment");
 var express = require("express");
 
 var app = express();
+
+var port = 8080;
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/:id', function(req, res) {
+  res.send('parameter = ' + req.params.id);
+});
+
+
+
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
+});
