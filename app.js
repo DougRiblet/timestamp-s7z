e1608a0a1a1a3dbc8811d99a090160b05595bff2
Doug Riblet <smashzen85@gmail.com>
2016-10-22 17:00:42 -0500

    set process.env.PORT for heroku deployment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require("express");
 var path    = require("path");
 
 var app = express();
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 
 app.get('/', function (req, res) {
