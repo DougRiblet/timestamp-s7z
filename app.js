5f5db1c581f691f81f4de951868f22fc6ae3f7e8
Doug Riblet <smashzen85@gmail.com>
2016-10-22 14:36:19 -0500

    Create index.html, add path module

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-
-
 var moment = require("moment");
 var express = require("express");
+var path    = require("path");
 
 var app = express();
-
 var port = 8080;
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.get('/:id', function(req, res) {
+	var newMoment;
 	if (moment(req.params.id).isValid()) {
-		res.send('valid parameter = ' + req.params.id);
-	} else {
-		res.send('invalid parameter = ' + req.params.id);
+		newMoment = moment(req.params.id);
+	} else if (moment(req.params.id, "x").isValid()) {
+		newMoment = moment(req.params.id, "x")
+	} else if (moment(req.params.id, "X").isValid()) {
+		newMoment = moment(req.params.id, "X");
 	}
+	var unixFormat = newMoment ? moment(newMoment).format("X") : null;
+	var naturalFormat = newMoment ? moment(newMoment).format("MMMM D, YYYY") : null;
+
 });
 
 
