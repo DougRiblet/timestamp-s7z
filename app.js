5e42055f61161879d7a3a0e221afd5acb4ee1c6d
Doug Riblet <smashzen85@gmail.com>
2016-10-23 12:17:23 -0500

    add favicon, limit unix timestamp format to seconds

--- app.js
+++ app.js
@@ -3,27 +3,29 @@ var express = require("express");
 var path    = require("path");
 
 var app = express();
-var port = process.env.PORT || 8080;
+var port = 8080;
+// process.env.PORT || 
 
 
 app.get('/', function (req, res) {
-	console.log("===========reaching app.get")
+	console.log("====basic get");
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.get('/:id', function(req, res) {
-	var newMoment;
-	if (moment(req.params.id).isValid()) {
-		newMoment = moment(req.params.id);
-	} else if (moment(req.params.id, "x").isValid()) {
-		newMoment = moment(req.params.id, "x")
-	} else if (moment(req.params.id, "X").isValid()) {
-		newMoment = moment(req.params.id, "X");
-	}
-	var unixFormat = newMoment ? moment(newMoment).format("X") : null;
-	var naturalFormat = newMoment ? moment(newMoment).format("MMMM D, YYYY") : null;
+	if (req.params.id.length) {
+		console.log("====param get: ", req.params.id);
+		var newMoment;
+		if (moment(req.params.id).isValid()) {
+			newMoment = moment(req.params.id);
+		} else if (moment(req.params.id, "X").isValid()) {
+			newMoment = moment(req.params.id, "X");
+		}
+		var unixFormat = newMoment ? moment(newMoment).format("X") : null;
+		var naturalFormat = newMoment ? moment(newMoment).format("MMMM D, YYYY") : null;
 
-	res.json({ 'unix': unixFormat, 'natural': naturalFormat });
+		res.json({ 'unix': unixFormat, 'natural': naturalFormat });
+	}
 });
 
 
